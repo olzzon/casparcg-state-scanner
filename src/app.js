eb10370004788ddd145996bdf254ffe565bb32ad
olzzon <github@olzzon.dk>
2018-10-13 21:36:06 +0200

    Recieve OSC - name info of 1-10 and serve it at :5254/api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,44 @@
 import { CcgStateOSC } from './casparCGStateOSC';
 const express = require('express');
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+//This is how a OSC message from CCG look like:
+/*
+        /channel/1/stage/layer/10/background/producer : <OSCVal s "empty">
+        /channel/1/stage/layer/10/foreground/file/name : <OSCVal s "go1080p25.mp4">
+        /channel/1/stage/layer/10/foreground/file/path : <OSCVal s "media/go1080p25.mp4">
+        /channel/1/stage/layer/10/foreground/file/streams/0/fps : (
+            "<OSCVal i 25>",
+            "<OSCVal i 1>"
+        )
+        /channel/1/stage/layer/10/foreground/file/streams/1/fps : (
+            "<OSCVal i 0>",
+            "<OSCVal i 0>"
+        )
+        /channel/1/stage/layer/10/foreground/file/time : (
+            "<OSCVal f 7.940000>",
+            "<OSCVal f 17.799999>"
+        )
+        /channel/1/stage/layer/10/foreground/loop : <OSCVal T>
+        /channel/1/stage/layer/10/foreground/paused : <OSCVal F>
+        /channel/1/stage/layer/10/foreground/producer : <OSCVal s "ffmpeg">
+*/
+
+//Build GraphQl Schemes:
+var apiSchema = buildSchema(`
+  type Query {
+    channelPlaying: Boolean
+    foregroundName: String
+    backgroundName: String
+  }
+`);
 
 export class App {
     constructor() {
         this.playing = false;
-        this.foregroundProducername = 'TEST OF NAME';
+        this.foregroundName = 'TEST OF FRONT';
+        this.backgroundName = 'TEST OF BACK';
         this.setupOscServer();
         this.setupExpressServer();
     }
@@ -12,19 +46,35 @@ export class App {
     setupOscServer() {
         const oscConnection = new CcgStateOSC({ plugin: new CcgStateOSC.WebsocketServerPlugin({ port: 5900 }) });
         oscConnection.connect(); 
-        oscConnection.on('', (message) => {
-            this.playing = true;
+        oscConnection.on('/channel/1/stage/layer/10/foreground/file/name', (message) => {
+            this.foregroundName = message.args;
             console.log(message.args);
         });
-        console.log(oscConnection);
+        oscConnection.on('/channel/1/stage/layer/10/background/file/name', (message) => {
+            this.backgroundName = message.args;
+            console.log(message.args);
+        });
     }
+
 
     setupExpressServer() {
         const server = express();
         const port = 5254;
 
-        server.get('/name', (req, res) => res.send(this.foregroundProducername));
-        server.get('/playing', (req, res) => res.send(this.playing));
+        //server.get('/name', (req, res) => res.send(this.foregroundProducername));
+        //server.get('/playing', (req, res) => res.send(this.playing));
+
+        // Root resolver
+        var graphQlRoot = {
+            channelPlaying: () => this.playing,
+            foregroundName: () => this.foregroundName,
+            backgroundName: () => this.backgroundName,
+        };
+        server.use('/api', express_graphql({
+            schema: apiSchema,
+            rootValue: graphQlRoot,
+            graphiql: true
+        }));
 
         server.listen(port, () => console.log(`Example app listening on port ${port}!`));
     }
